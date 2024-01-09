ae03638e0cea43a6a0b8d3864a6968fed4fe9a8c
johnbedeir <john.bedeir@gmail.com>
2024-01-09 01:05:49 +0100

    Fix #21

--- build.sh
+++ build.sh
@@ -39,36 +39,36 @@ cd ..
 echo "--------------------Update Kubeconfig--------------------"
 aws eks update-kubeconfig --name $cluster_name --region $region
 
-# # remove preious docker images
+# remove preious docker images
 echo "--------------------Remove Previous build--------------------"
 docker rmi -f $app_image_name || true
 docker rmi -f $db_image_name || true
 
-# # # build new docker image with new tag
+# build new docker image with new tag
 echo "--------------------Build new Image--------------------"
-docker build -t $app_image_name todo-app/
-docker build -f k8s/Dockerfile.mysql -t $db_image_name k8s/
+# docker build -t $app_image_name todo-app/
+docker build -t $db_image_name -f k8s/Dockerfile.mysql k8s
 
-# # #ECR Login
+# ECR Login
 echo "--------------------Login to ECR--------------------"
 aws ecr get-login-password --region $region | docker login --username AWS --password-stdin $aws_id.dkr.ecr.eu-central-1.amazonaws.com
 
-# # # push the latest build to dockerhub
+# push the latest build to dockerhub
 echo "--------------------Pushing Docker Image--------------------"
 docker push $app_image_name
 docker push $db_image_name
 
-# # create namespace
+# create namespace
 echo "--------------------creating Namespace--------------------"
 kubectl create ns $namespace || true
 
-# # add rds endpoint into k8s secrets
+# add rds endpoint into k8s secrets
 echo "--------------------Create RDS Secrets --------------------"
-kubectl create secret generic rds-endpoint -n $namespace --from-literal=endpoint=$rds_endpoint || true
-kubectl create secret generic rds-username -n $namespace --from-literal=username=$db_username || true
-kubectl create secret generic rds-password -n $namespace --from-literal=password=$db_password || true
+kubectl create secret -n $namespace generic rds-endpoint --from-literal=endpoint=$rds_endpoint || true
+kubectl create secret -n $namespace generic rds-username --from-literal=username=$db_username || true
+kubectl create secret -n $namespace generic rds-password --from-literal=password=$db_password || true
 
-# # Deploy the application
+# deploy the application
 echo "--------------------Deploy App--------------------"
 kubectl apply -n $namespace -f k8s/
 
@@ -85,3 +85,7 @@ echo "--------------------Prometheus LoadBalancer URL--------------------"
 kubectl get svc -n ${monitoring_ns} ${prometheus_service_name} -o=custom-columns=EXTERNAL-IP:.status.loadBalancer.ingress[*].hostname | tail -n +2
 echo "--------------------Grafana LoadBalancer URL--------------------"
 kubectl get svc -n ${monitoring_ns} ${grafana_service_name} -o=custom-columns=EXTERNAL-IP:.status.loadBalancer.ingress[*].hostname | tail -n +2
+
+# Get RDS endpoint URL
+echo "--------------------RDS endpoint URL--------------------"
+echo $rds_endpoint
